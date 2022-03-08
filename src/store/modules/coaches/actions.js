9dc6f6ca8a33f19cb6245e38b44a9e242b61e063
Ares Hovsepyan <areshovsepyan@yahoo.com>
2022-03-09 02:28:24 +0400

    PUT http request - store coach data implemented

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,13 +1,28 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
     const coachData = {
-      id: context.rootGetters.userId,
       firstName: data.firstName,
       lastName: data.lastName,
       description: data.description,
       hourlyRate: data.rate,
       areas: data.areas,
     };
-    context.commit('registerCoach', coachData);
+
+    const response = await fetch(
+      `https://find-a-coach-39d45-default-rtdb.firebaseio.com/coaches/${userId}.json`,
+      {
+        method: 'PUT',
+        body: JSON.stringify(coachData),
+      }
+    );
+
+    // const responseData = await response.json();
+
+    if (!response.ok) {
+      //error
+    }
+
+    context.commit('registerCoach', { ...coachData, id: userId });
   },
 };
