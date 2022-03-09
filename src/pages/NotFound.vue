5c2ad833433fc35baf7d99bed39fc64c1747075d
Ares Hovsepyan <areshovsepyan@yahoo.com>
2022-03-09 17:57:25 +0400

    notFound page implemented

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>
+        This page could not be found. Check out all our
+        <router-link to="/coaches">coaches.</router-link>
+      </p>
+    </base-card>
+  </section>
+</template>
+
+<style scoped>
+a {
+  color: #3e94d1;
+  text-decoration: none;
+}
+a:active,
+a:hover {
+  text-decoration: underline;
+}
+</style>
